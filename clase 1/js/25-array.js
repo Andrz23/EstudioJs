ca68cd6b8bb5a075b43b2c5cb0d0ad041598bc9d
Andres Morales <mrfelipe32@gmail.com>
2019-07-26 10:25:48 -0500

    Se suben nuevo cambios

--- clase 1/js/25-array.js
+++ clase 1/js/25-array.js
@@ -16,9 +16,31 @@ document.write("<ul>");
 // }
 
 //forma de recorrear el array en js
-lenguajes.forEach((elemento, indice, data)=>{
+/*lenguajes.forEach((elemento, indice, data)=>{
     document.write("<li>"+ indice +""+elemento+ "</li>");
-});
+});*/
 
+//forma de recorrer de arrar for in 
+for(let lenguaje in lenguajes){
+    document.write("<li>"+ lenguajes[lenguaje]+"</li>");
+}
 document.write("</ul>");
 
+
+//busquedas 
+
+/* var busqueda = lenguajes.find(function(lenguaje){
+return lenguaje =="PHP7";
+});*/
+
+
+//forma de hacerlo
+
+var numeros= [10,20,50,12,11];
+var busqueda = numeros.some(precio => precio >=60);
+
+//var busqueda = lenguajes.find(lenguaje => lenguaje =="PHP");
+
+
+
+console.log(busqueda);
--- clase 1/js/26-arrayMulti.js
+++ clase 1/js/26-arrayMulti.js
@@ -13,20 +13,40 @@ var categoria=["Accion", "Terror","Comedia"];
 
 var anime=["One piece", "Naruto", "Dragon ball"];
 
+
+//Para ordenar el array sort(), reverse()
+anime.reverse();
+console.log(anime);
+
 var cine= [categoria, anime];
 
 //console.log(cine[0][1]);
 //console.log(cine[1][2]);
 
 var elemento ="";
-do{
-    elemento = prompt("Introduce tu anime");
-    anime.push(elemento);
-}while(elemento!= "Terminar");
+// do{
+//     elemento = prompt("Introduce tu anime");
+//     anime.push(elemento);
+// }while(elemento!= "Terminar");
+
+
+var indice = anime.indexOf("Dragon ball");
+
+if(indice > -1){
+    anime.splice(indice, 1);
+} 
 
 
 //elimina el ultimo elemento del array
-anime.pop();
+//anime.pops();
 
 
-console.log(anime);
+// Para convertir el array en un String por medio de (,);
+var animes_string = anime.join();
+
+
+// para convertir un String a un array 
+var cadena = "texto1,texto2,texto3";
+var cadena_array = cadena.split(",");
+
+console.log(cadena_array);
